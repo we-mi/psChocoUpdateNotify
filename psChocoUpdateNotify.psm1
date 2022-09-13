b09a0eb24a13f820bc0b8eacf96f8cf34795d003
Michael Wenders <1871010+we-mi@users.noreply.github.com>
2022-09-13 22:05:00 +0200

    Added a powershell module. Use 'Start-PSChocoUpdateNotify' to start the application when the module is installed

---
+++ psChocoUpdateNotify.psm1
@@ -0,0 +1,11 @@
+﻿function Start-PSChocoUpdateNotify {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]
+        [ValidateSet("GUI","Notification")]
+        $Mode = "Notification"
+    )
+
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-file `"$(Join-Path $PSScriptRoot "psChocoUpdateNotify.ps1")`" -Mode $Mode" -WindowStyle "Hidden"
+}
